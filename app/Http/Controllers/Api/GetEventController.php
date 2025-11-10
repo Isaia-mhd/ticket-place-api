7bc9035a8ab012f6fbc72346b0747dfde151ff28
Isaia-mhd <mohamedesaie21@gmail.com>
2025-11-10 17:17:30 +0300

    fix: add the filtering by date of events

--- app/Http/Controllers/Api/GetEventController.php
+++ app/Http/Controllers/Api/GetEventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class GetEventController extends Controller
@@ -17,6 +18,7 @@ class GetEventController extends Controller
                 $query->where('title', 'like', "%{$search}%")
                     ->orWhere('description', 'like', "%{$search}%")
                     ->orWhere('venue', 'like', "%{$search}%")
+                    ->orWhereDate('event_date', Carbon::parse($search)->format('Y-m-d'))
                     ->orWhere('type', 'like', "%{$search}%");
             })
             ->latest()
